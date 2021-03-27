b92339f8b3ad6ce4a0a6e41099424793666f452d
sammy-code98 <ugosammy98@gmail.com>
2021-03-27 11:18:22 +0100

    made nav bar responsive

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function navBar() {
+    // console.log('hello');
+
+    let x = document.getElementById('navbar-btn')
+    if (x.style.display === 'block') {
+        x.style.display = 'none'
+    } else {
+        x.style.display = 'block'
+    }
+}
